5539004687df28f093d12ad81e29ad3131d90126
wschutz@gmail.com <wschutz@gmail.com@ca5865f0-ea67-0c57-7e18-94a3701c88e0>
2011-03-08 18:03:53 +0000

    Some more methods on TriplesList
    git-svn-id: http://hdt-it.googlecode.com/svn/trunk@78 ca5865f0-ea67-0c57-7e18-94a3701c88e0

--- hdt-lib/src/triples/TriplesList.cpp
+++ hdt-lib/src/triples/TriplesList.cpp
@@ -24,7 +24,81 @@ IteratorTripleID *TriplesList::search(const TripleID &pattern)
 {
 	std::vector<TripleID *>::iterator it = this->arrayOfTriples.begin();
 	return new IteratorTripleID(it, pattern);
+} // search()
+
+float TriplesList::cost(const TripleID &pattern)
+{
+	// TODO: Theoretically define this with the team
+	return -1;
+} // cost()
+
+unsigned int TriplesList::getNumberOfElements()
+{
+	return (unsigned int) this->arrayOfTriples.size();
+} // getNumberOfElements()
+
+unsigned int TriplesList::size()
+{
+	return (unsigned int) this->arrayOfTriples.size()*sizeof(TripleID);
+	//return (unsigned int) this->getNumberOfElements()*sizeof(TripleID);
+} // size()
+
+bool TriplesList::save(const std::ostream &output)
+{
+	// TODO
+	return false;
 }
 
+void TriplesList::load(const std::istream &input, const Header &header)
+{
+	// TODO
+}
+
+void TriplesList::load(const ModifiableTriples &input)
+{
+	// TODO
+}
+
+void TriplesList::populateHeader(const Header &header)
+{
+	// TODO
+}
+
+void TriplesList::startProcessing()
+{
+	// TODO
+}
+
+void TriplesList::stopProcessing()
+{
+	// TODO
+}
+
+
+// From ModifiableTriples
+
+bool TriplesList::insert(const TripleID &triple)
+{
+	// Saving the size of the list of triples before inserting anything
+	unsigned int size = this->arrayOfTriples.size();
+
+	TripleID temp = triple;
+
+	// Add the triple
+	arrayOfTriples.push_back(&temp);
+
+	// Verify the triple has been added (sort of assert...)
+	if (this->arrayOfTriples.size() != (size+1)) {
+		return false;
+	}
+	// TODO: Retrieve the last triple added and compare it with the parameter
+
+	return true;
+}
+
+bool TriplesList::insert(const IteratorTripleID *triples)
+{
+	return false;
+}
 
 } // hdt{}
--- hdt-lib/src/triples/TriplesList.hpp
+++ hdt-lib/src/triples/TriplesList.hpp
@@ -1,5 +1,5 @@
 /*
- * TriplesList.h
+ * TriplesList.hpp
  *
  *  Created on: 04/03/2011
  *      Author: billy
@@ -15,17 +15,124 @@ namespace hdt {
 class TriplesList : public ModifiableTriples {
 	private:
 		std::vector<TripleID *> arrayOfTriples;
+		TripleComponentOrder order;
 
 	public:
 		TriplesList();
 		virtual ~TriplesList();
+
+		// From Triples
+
 		/**
-		 * Returns a vector of triples matching the pattern
+	     * Returns a vector of triples matching the pattern
+	     *
+	     * @param pattern
+	     * @return
+	     */
+		IteratorTripleID *search(const TripleID &pattern);
+
+		/**
+		 * Calculates the cost to retrieve a specific pattern
+	     *
+	     * @param triple
+	     * @return
+	     */
+		float cost(const TripleID &triple);
+
+		/**
+	     * Returns the number of triples contained
+	     *
+	     * @return
+	     */
+		unsigned int getNumberOfElements();
+
+		/**
+		 * Returns size in bytes
+		 */
+		unsigned int size();
+
+		/**
+	     * Saves the triples
+	     *
+	     * @param output
+	     * @return
+	     */
+		bool save(const std::ostream &output);
+
+		/**
+	     * Loads triples from a file
+	     *
+	     * @param input
+	     * @return
+	     */
+		void load(const std::istream &input, const Header &header);
+
+		void load(const ModifiableTriples &input);
+
+		/**
+		 * Populates the header
 		 *
-		 * @param pattern
+		 * @param header
 		 * @return
 		 */
-		IteratorTripleID *search(const TripleID &pattern);
+		void populateHeader(const Header &header);
+
+		/**
+		 * TODO Define and decide on return type (bool?)
+		 */
+		void startProcessing();
+
+		/**
+		 * TODO Define and decide on return type (bool?)
+		 */
+		void stopProcessing();
+
+		// From ModifiableTriples
+
+		/**
+		 * Adds one or more triples
+		 *
+		 * @param triples The triples to be inserted
+		 * @return boolean
+		 */
+		bool insert(const TripleID &triple);
+
+		bool insert(const IteratorTripleID *triples);
+
+		/**
+		 * Deletes one or more triples according to a pattern
+		 *
+		 * @param pattern
+		 *            The pattern to match against
+		 * @return boolean
+		 */
+		bool remove(TripleID &pattern);
+
+		bool remove(IteratorTripleID *pattern);
+
+		/**
+		 * Updates a triple with new components
+		 *
+		 * @param oldTriple The triple to be replaced
+		 * @param newTriple The triple to replace the old one
+		 * @return boolean
+		 */
+		bool edit(TripleID &oldTriple, TripleID &newTriple);
+
+		/**
+		 * Sorts the triples based on an order(TripleComponentOrder)
+		 *
+		 * @param order The order to sort the triples with
+		 */
+		void sort(TripleComponentOrder order);
+
+		/**
+		 * Sets a type of order(TripleComponentOrder)
+		 *
+		 * @param order The order to set
+		 */
+		void setOrder(TripleComponentOrder order);
+
 
 };
 
