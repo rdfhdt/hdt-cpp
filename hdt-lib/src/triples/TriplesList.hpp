2e968fcb4b192f6ed3548a8bde8c580a169b0108
wschutz@gmail.com <wschutz@gmail.com@ca5865f0-ea67-0c57-7e18-94a3701c88e0>
2011-04-01 00:54:47 +0000

    Fix: Adding svn:keywords "Id" property the proper way... (testing the feature for now)
    git-svn-id: http://hdt-it.googlecode.com/svn/trunk@92 ca5865f0-ea67-0c57-7e18-94a3701c88e0

--- hdt-lib/src/triples/TriplesList.hpp
+++ hdt-lib/src/triples/TriplesList.hpp
@@ -1,8 +1,36 @@
 /*
  * TriplesList.hpp
  *
- *  Created on: 04/03/2011
- *      Author: billy
+ * Copyright (C) 2011, Javier D. Fernandez, Miguel A. Martinez-Prieto
+ *                     Guillermo Rodriguez-Cano, Alejandro Andres,
+ *                     Mario Arias
+ * All rights reserved.
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * Contacting the authors:
+ *   Javier D. Fernandez:       jfergar@infor.uva.es
+ *   Miguel A. Martinez-Prieto: migumar2@infor.uva.es
+ *   Guillermo Rodriguez-Cano:  wileeam@acm.org
+ *   Alejandro Andres:          fuzzy.alej@gmail.com
+ *   Mario Arias:               mario.arias@gmail.com
+ *
+ * @version $Id$
+ *
  */
 
 #ifndef TRIPLESLIST_H_
