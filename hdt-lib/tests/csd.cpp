7f6cb7840843cc17dedee0b04a89a2f01333d1d8
mario.arias <mario.arias@ca5865f0-ea67-0c57-7e18-94a3701c88e0>
2012-08-21 20:30:28 +0000

    Updates on tests.
    git-svn-id: http://hdt-it.googlecode.com/svn/trunk@254 ca5865f0-ea67-0c57-7e18-94a3701c88e0

---
+++ hdt-lib/tests/csd.cpp
@@ -0,0 +1,92 @@
+/*
+ * Tutorial01.cpp
+ *
+ *  Created on: 02/03/2011
+ *      Author: mck
+ */
+
+#include <HDT.hpp>
+#include <HDTFactory.hpp>
+#include <Header.hpp>
+#include <Dictionary.hpp>
+#include <Triples.hpp>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "../src/libdcs/CSD_PFC.h"
+
+#include "../src/util/StopWatch.hpp"
+
+using namespace hdt;
+using namespace csd;
+using namespace std;
+
+class ConvertProgress : public ProgressListener {
+private:
+public:
+	virtual ~ConvertProgress() { }
+
+    void notifyProgress(float level, const char *section) {
+    	cout << section << ": " << level << " %";
+    	cout << "\r " << section << ": " << level << " %                      \r";
+		cout.flush();
+	}
+
+};
+
+int main(int argc, char **argv) {
+	int c;
+	string query, inputFile, outputFile;
+	bool measure = false;
+
+	while( (c = getopt(argc,argv,"hq:o:m"))!=-1) {
+		switch(c) {
+		case 'h':
+			break;
+		case 'q':
+			query = optarg;
+			break;
+		case 'o':
+			outputFile = optarg;
+			break;
+		case 'm':
+			measure = true;
+			break;
+		default:
+			cout << "ERROR: Unknown option" << endl;
+			return 1;
+		}
+	}
+
+	if(argc-optind<1) {
+		cout << "ERROR: You must supply an input and HDT File" << endl << endl;
+		return 1;
+	}
+
+	inputFile = argv[optind];
+
+	try {
+
+		ConvertProgress progress;
+		// CONVERT
+		FileteratorUCharString iterator(inputFile);
+
+		CSD_PFC *csd = new CSD_PFC(&iterator, 32, &progress);
+
+		ofstream out(outputFile.c_str(), ios::binary);
+		csd->save(out);
+		out.close();
+
+		delete csd;
+
+	} catch (char *e) {
+		cout << "ERROR: " << e << endl;
+	} catch (const char *e) {
+		cout << "ERROR: " << e << endl;
+	}
+}
+
+
+
---
+++ hdt-lib/tests/opendic.cpp
@@ -0,0 +1,88 @@
+/*
+ * Tutorial01.cpp
+ *
+ *  Created on: 02/03/2011
+ *      Author: mck
+ */
+
+#include <HDT.hpp>
+#include <HDTFactory.hpp>
+#include <Header.hpp>
+#include <Dictionary.hpp>
+#include <Triples.hpp>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "../src/dictionary/PFCDictionary.hpp"
+
+#include "../src/util/StopWatch.hpp"
+
+using namespace hdt;
+using namespace std;
+
+class ConvertProgress : public ProgressListener {
+private:
+public:
+	virtual ~ConvertProgress() { }
+
+    void notifyProgress(float level, const char *section) {
+    	cout << section << ": " << level << " %";
+    	cout << "\r " << section << ": " << level << " %                      \r";
+		cout.flush();
+	}
+
+};
+
+int main(int argc, char **argv) {
+	int c;
+	string query, inputFile, outputFile;
+	bool measure = false;
+
+	while( (c = getopt(argc,argv,"hq:o:m"))!=-1) {
+		switch(c) {
+		case 'h':
+			break;
+		case 'q':
+			query = optarg;
+			break;
+		case 'o':
+			outputFile = optarg;
+			break;
+		case 'm':
+			measure = true;
+			break;
+		default:
+			cout << "ERROR: Unknown option" << endl;
+			return 1;
+		}
+	}
+
+	if(argc-optind<1) {
+		cout << "ERROR: You must supply an input and HDT File" << endl << endl;
+		return 1;
+	}
+
+	inputFile = argv[optind];
+
+	try {
+
+		// CONVERT
+		ConvertProgress progress;
+		ControlInformation ci;
+		PFCDictionary dict;
+
+		ifstream in(inputFile.c_str(), ios::binary);
+
+		ci.load(in);
+		dict.load(in, ci, &progress);
+	} catch (char *e) {
+		cout << "ERROR: " << e << endl;
+	} catch (const char *e) {
+		cout << "ERROR: " << e << endl;
+	}
+}
+
+
+
