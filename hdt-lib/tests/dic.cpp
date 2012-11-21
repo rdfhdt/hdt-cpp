5781219e503dfdfad8b8480b9a47d649e84105a1
mario.arias <mario.arias@ca5865f0-ea67-0c57-7e18-94a3701c88e0>
2012-11-21 20:24:08 +0000

    Added some tests
    git-svn-id: http://hdt-it.googlecode.com/svn/trunk@298 ca5865f0-ea67-0c57-7e18-94a3701c88e0

---
+++ hdt-lib/tests/dic.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <HDTFactory.hpp>
+
+using namespace hdt;
+
+int main(int argc, char **argv) {
+
+	try {
+		HDT *hdt = HDTFactory::mapHDT(argv[1]);
+		IteratorUCharString *it =hdt->getDictionary()->getObjects();
+
+		ofstream lit("lit.txt");
+		ofstream blk("blank.txt");
+		ofstream uri("uri.txt");
+		while(it->hasNext()) {
+		    unsigned char *str = it->next();
+		    if(*str=='"') {
+			  // Literal
+			lit << (char*)str << endl;
+		    } else if(*str=='_'){
+			 // Blanco
+			blk << (char*)str << endl;
+		    } else {
+			 // URI
+			uri << (char*)str << endl;
+		    }
+		}
+		lit.close();
+		blk.close();
+		uri.close();
+
+		delete it;
+		delete hdt;
+	 } catch(const char *str) {
+		cerr << str << endl;
+	 } catch(char *str) {
+		cerr << str << endl;
+	 }
+
+}
