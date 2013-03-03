60486bcd581afef5aad7b7bbbe790437eb84110b
mario.arias <mario.arias@ca5865f0-ea67-0c57-7e18-94a3701c88e0>
2013-03-03 11:32:02 +0000

    Added loop to show evolution of a file
    git-svn-id: http://hdt-it.googlecode.com/svn/trunk@378 ca5865f0-ea67-0c57-7e18-94a3701c88e0

--- hdt-lib/tests/mincor.cpp
+++ hdt-lib/tests/mincor.cpp
@@ -6,6 +6,18 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+#include <signal.h>
+
+
+int interruptSignal = 0;
+
+void signalHandler(int sig)
+{
+        interruptSignal = 1;
+}
+
+
+
 #ifdef __APPLE__
 typedef char mincore_arr;
 #else
@@ -21,21 +33,31 @@ int main(int argc, char *argv[]) {
     size_t page_index;
     size_t numMemory;
     size_t totalPages;
+    unsigned long count=0;
 
     fd = open(argv[1],0);
     fstat(fd, &file_stat);
     file_mmap = mmap((void *)0, file_stat.st_size, PROT_NONE, MAP_SHARED, fd, 0);
     mincore_vec = (mincore_arr *)calloc(1, (file_stat.st_size+page_size-1)/page_size);
-    mincore((void *)file_mmap, file_stat.st_size, mincore_vec);
-    printf("Cached Blocks of %s: \n",argv[1]);
-    for (page_index = 0; page_index <= file_stat.st_size/page_size; page_index++) {
-    	if (mincore_vec[page_index]&1) {
-    		//printf("%lu ", (unsigned long)page_index);
-    		numMemory++;
-    	}
-    	totalPages++;
-    }
-    printf("Pages in memory %lu / %lu  Percent: %lu \n", (unsigned long)numMemory, (unsigned long)totalPages, (unsigned long)(100UL*numMemory/totalPages));
+    printf("Cached Blocks of %s, Page size: %lu: \n",argv[1], (unsigned long)page_size);
+
+
+    signal(SIGINT, &signalHandler);
+	while(interruptSignal==0) {
+		numMemory=totalPages=0;
+		count++;
+	    mincore((void *)file_mmap, file_stat.st_size, mincore_vec);
+	    for (page_index = 0; page_index <= file_stat.st_size/page_size; page_index++) {
+		if (mincore_vec[page_index]&1) {
+			//printf("%lu ", (unsigned long)page_index);
+			numMemory++;
+		}
+		totalPages++;
+	    }
+	    printf("Pages in memory %lu / %lu  Percent: %lu \n", (unsigned long)numMemory, (unsigned long)totalPages, (unsigned long)(100UL*numMemory/totalPages));
+	    //printf("%lu,%lu,%lu\n", count, (unsigned long)numMemory, (unsigned long)totalPages);
+	    sleep(1);
+	}
     free(mincore_vec);
     munmap(file_mmap, file_stat.st_size);
     close(fd);
