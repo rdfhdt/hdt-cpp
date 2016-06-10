rdfhdt/hdt-cpp

commit e1434df809754f5938b382b34abe8fa916e83557

Fixed -Wall compiler warnings about unused local variables.

hdt-lib/src/libdcs/CSD_FMIndex.cpp
@@ -59,12 +59,12 @@ CSD_FMIndex::CSD_FMIndex(hdt::IteratorUCharString *it, bool sparse_bitsequence, 
 	size_t len = 0;
 	size_t reservedSize = 1024;
 	text = (unsigned char*) malloc(reservedSize * sizeof(unsigned char));
-	std:vector < size_t > samplingsPositions;
+	std::vector < size_t > samplingsPositions;
 
 	text[0] = '\1'; //We suppose that \1 is not part of the text
 	maxlength = 0;
 	numstrings = 0;
-	uint m_l = 0;
+	//uint m_l = 0;
 
 	size_t total = 1;
 
@@ -292,7 +292,7 @@ size_t CSD_FMIndex::load(unsigned char *ptr, unsigned char *ptrMax)
     std::stringstream localStream;
     localStream.rdbuf()->pubsetbuf((char*)ptr, ptrMax-ptr);
 
-    unsigned char type = localStream.get(); // Load expects the type already read.
+    //unsigned char type = localStream.get(); // Load expects the type already read.
 
     this->type = FMINDEX;
     this->numstrings = loadValue<uint32_t>(localStream);
@@ -365,7 +365,7 @@ void csd::CSD_FMIndex::fillSuggestions(const char *base,
         vector<std::string> &out, int maxResults) {
     size_t len = strlen(base);
     unsigned char *n_s = new unsigned char[len + 1];
-    uint o;
+    //uint o;
     n_s[0] = '\1';
     for (uint32_t i = 1; i <= len; i++)
         n_s[i] = base[i - 1];

hdt-lib/src/libdcs/CSD_HTFC.cpp
@@ -151,7 +151,7 @@ CSD_HTFC::CSD_HTFC(hdt::IteratorUCharString *it, uint32_t blocksize, hdt::Progre
 
 	// Auxiliar variables for Hu-Tucker encoding
 	uint offset = 0, cblocks = 0;
-	uint64_t i = 0, slength=0;
+	uint64_t i = 0/*, slength=0*/;
 
 	while (i < bytesfc)
 	{

hdt-lib/src/sequence/HuffmanSequence.cpp
@@ -120,9 +120,9 @@ void HuffmanSequence::save(std::ostream & output)
 	// Calculate size
 	unsigned int numElements = vectorPlain.size();
 	unsigned int encodedEntries = 1+pos/(sizeof(unsigned int)*8);
-	unsigned int bytes = 1+pos/8;
 
 #if 0
+	unsigned int bytes = 1+pos/8;
 	cout << "Total bits: " << pos << endl;
 	cout << "Total entries: " << encodedEntries << endl;
 	cout << "Total bytes: " << bytes << endl;

hdt-lib/src/triples/PlainTriples.cpp
@@ -143,7 +143,7 @@ void PlainTriples::load(std::istream &input, ControlInformation &controlInformat
 		throw "Trying to read PlainTriples but the data is not PlainTriples";
 	}
 
-	unsigned int numTriples = controlInformation.getUint("numTriples");
+	//unsigned int numTriples = controlInformation.getUint("numTriples");
 	order = (TripleComponentOrder) controlInformation.getUint("order");
 
 	IntermediateListener iListener(listener);

hdt-lib/src/triples/TriplesList.cpp
@@ -137,7 +137,7 @@ size_t TriplesList::load(unsigned char *ptr, unsigned char *ptrMax, ProgressList
     }
 
     order = (TripleComponentOrder) controlInformation.getUint("order");
-    unsigned long long totalTriples = controlInformation.getUint("numTriples");
+    //unsigned long long totalTriples = controlInformation.getUint("numTriples");
     this->numValidTriples = 100000000;
 
     //CHECKPTR(&ptr[count],ptrMax,numValidTriples*12);

commit 17f5ce8f7a9d840097cc0b44738a3549546888b2

Fix CSD index reading.
Corrects a bug introduced in e1434df809754f5938b382b34abe8fa916e83557.

hdt-lib/src/libdcs/CSD_FMIndex.cpp
@@ -292,7 +292,7 @@ size_t CSD_FMIndex::load(unsigned char *ptr, unsigned char *ptrMax)
     std::stringstream localStream;
     localStream.rdbuf()->pubsetbuf((char*)ptr, ptrMax-ptr);
 
-    //unsigned char type = localStream.get(); // Load expects the type already read.
+    localStream.get(); // Load expects the type already read.
 
     this->type = FMINDEX;
     this->numstrings = loadValue<uint32_t>(localStream);