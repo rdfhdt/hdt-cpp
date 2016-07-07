53192d604a7a06834430b9569ae5b232b560f4f3
Miel Vander Sande <miel.vandersande@ugent.be>
2016-07-07 14:49:07 +0200

    Cleaned and added documentation

--- hdt-lib/include/HDTVersion.hpp
+++ hdt-lib/include/HDTVersion.hpp
@@ -1,45 +1,39 @@
-#define HDT_VERSION "1"
-#define INDEX_VERSION "1"
-#define RELEASE_VERSION "1"
 /*
-using namespace std;
+ * File: HDTVersion.cpp
+ * Last modified: $Date$
+ * Revision: $Revision$
+ * Last modified by: $Author$
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contacting the authors:
+ *   Mario Arias:               mario.arias@gmail.com
+ *   Javier D. Fernandez:       jfergar@infor.uva.es
+ *   Miguel A. Martinez-Prieto: migumar2@infor.uva.es
+ *
+ * This file determines the current version of the software.
+ * The total version number is: v<HDT_version>.<INDEX_VERSION>.<RELEASE_VERSION>
+ */
 
-namespace hdt {
-  // Parses version URI
-  inline std::vector<int> parse_version_uri(std::string uri) {
-    std::string version_base = HDTVocabulary::HDT_BASE+"HDTv";
-    std::size_t found = uri.find(version_base);
-    if (found==std::string::npos)
-      throw "Invalid version URI";
+// Version of the actual HDT file that is generated or read.
+// Software must be backwards compatible with all HDT files with the same number.
+#define HDT_VERSION "1"
 
-    std::cout << "first 'needle' found at: " << found << '\n';
+// Version of the accompagning .index file that is generated or read
+// Software must be backwards compatible with all index files with the same index and HDT version number.
+#define INDEX_VERSION "1"
 
-    uri.substr(version_base.length(), uri.length() - version_base.length() - 1); // remove trailing ">"
-
-    // Parse version number
-    // First number is HDT, second is Index, Third is release
-    std::vector<int> result(3);
-    std::string delimiter = "-";
-    // Find first
-    found = uri.find(delimiter);
-    if (!found)
-      throw "Invalid version URI";
-
-    int hdt_version = stoi(uri.substr(0, found));
-
-    found = uri.find(delimiter, found + 1);
-    if (!found)
-      return { hdt_version, 0, 0 };
-
-    int index_version = stoi(uri.substr(0, found));
-
-    found = uri.find(delimiter, found + 1);
-    if (!found)
-      return { hdt_version, index_version, 0 };
-
-    int release_version = stoi(uri.substr(0, found));
-
-    return { hdt_version, index_version, 0 };
-  }
-}
-*/
+// Subreleases that are backwards compatible with both HDT and index file
+#define RELEASE_VERSION "1"
