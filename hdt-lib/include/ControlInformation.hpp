rdfhdt/hdt-cpp

commit c96fed326b139ef8d8ddbdab836c5ed876b284d4

Made the hdt::ControlInformation interface const-correct (#18).

hdt-lib/include/ControlInformation.hpp
@@ -83,35 +83,37 @@ public:
 
     size_t load(const unsigned char *ptr, const unsigned char *maxPtr);
 
-    std::string getFormat();
+    const std::string& getFormat() const;
 
-    void setFormat(std::string format);
+    void setFormat(const std::string& format);
 
 	/** Get a property of the ControlInformation
 	 * @param key
 	 * @return
+	 * @throws std::out_of_range if the key is not found
 	 */
-	std::string get(std::string key);
+	const std::string& get(const std::string& key) const;
 
 	/** Get a property of the ControlInformation as unsigned int
 	 * @param key
 	 * @return
+	 * @throws std::out_of_range if the key is not found
 	 */
-    uint64_t getUint(std::string key);
+    uint64_t getUint(const std::string& key) const;
 
 	/**
 	 * Set a property of the ControlInformation
 	 * @param key
 	 * @param value
 	 */
-	void set(std::string key, std::string value);
+	void set(const std::string& key, const std::string& value);
 
 	/**
 	 * Set a property of the ControlInformation as unsigned int
 	 * @param key
 	 * @param value
 	 */
-    void setUint(std::string key, uint64_t value);
+    void setUint(const std::string& key, uint64_t value);
 
 	/** Clear the ControlInformation, removing all properties.
 	 */
@@ -119,9 +121,9 @@ public:
 
     void setType(ControlInformationType type);
 
-    bool isDefined(std::string key);
+    bool isDefined(const std::string& key) const;
 
-    ControlInformationType getType();
+    ControlInformationType getType() const;
 };
 
 }

hdt-lib/src/hdt/ControlInformation.cpp
@@ -33,28 +33,23 @@
 #include "ControlInformation.hpp"
 #include "../util/crc16.h"
 
-
 using namespace std;
 
 namespace hdt {
 
-ControlInformation::ControlInformation() : type(UNKNOWN_CI) {
+ControlInformation::ControlInformation() : type(UNKNOWN_CI) {}
 
-}
-
-ControlInformation::~ControlInformation() {
-
-}
+ControlInformation::~ControlInformation() {}
 
 void ControlInformation::save(std::ostream &out) {
 	CRC16 crc;
-	unsigned char null='\0';
+	const unsigned char null='\0';
 
     // Write cookie
 	crc.writeData(out, (unsigned char*)"$HDT", 4);
 
     // Write type
-    uint8_t typeValue = (uint8_t) type;
+    const uint8_t typeValue = (uint8_t) type;
     crc.writeData(out, (unsigned char *)&typeValue, sizeof(uint8_t));
 
     // Write format
@@ -79,7 +74,7 @@ void ControlInformation::save(std::ostream &out) {
 
 void ControlInformation::load(std::istream &in) {
 	CRC16 crc;
-    unsigned char null='\0';
+    const unsigned char null='\0';
 
     this->clear();
 
@@ -119,7 +114,7 @@ void ControlInformation::load(std::istream &in) {
     crc.update((unsigned char *)line.c_str(), line.length());
     crc.update(&null, 1);
 
-    crc16_t filecrc = crc16_read(in);
+    const crc16_t filecrc = crc16_read(in);
 
     // CRC16
     if(filecrc!=crc.getValue()) {
@@ -181,7 +176,7 @@ size_t ControlInformation::load(const unsigned char *ptr, const unsigned char *m
 	CRC16 crc;
 	crc.update(&ptr[0], count);
 	CHECKPTR(ptr,maxPtr, sizeof(crc16_t));
-	crc16_t filecrc = *((crc16_t *)&ptr[count]);
+	const crc16_t filecrc = *((crc16_t *)&ptr[count]);
 	if(filecrc!=crc.getValue()) {
 		throw "CRC of control information does not match.";
 	}
@@ -197,28 +192,27 @@ void ControlInformation::clear() {
 }
 
 
-std::string ControlInformation::getFormat() {
+const std::string& ControlInformation::getFormat() const {
 	return format;
 }
 
-void ControlInformation::setFormat(std::string format) {
+void ControlInformation::setFormat(const std::string& format) {
 	this->format = format;
 }
 
-std::string ControlInformation::get(std::string key) {
-	return map[key];
+const std::string& ControlInformation::get(const std::string& key) const {
+	return map.at(key);
 }
 
-void ControlInformation::set(std::string key, std::string value) {
+void ControlInformation::set(const std::string& key, const std::string& value) {
 	map[key] = value;
 }
 
-uint64_t ControlInformation::getUint(std::string key) {
-	std::string str = map[key];
-    return strtoull(str.c_str(), NULL, 10);
+uint64_t ControlInformation::getUint(const std::string& key) const {
+    return strtoull(map.at(key).c_str(), NULL, 10);
 }
 
-void ControlInformation::setUint(std::string key, uint64_t value) {
+void ControlInformation::setUint(const std::string& key, uint64_t value) {
 	std::stringstream out;
 	out << value;
 	map[key] = out.str();
@@ -228,12 +222,11 @@ void ControlInformation::setType(ControlInformationType type) {
     this->type = type;
 }
 
-
-bool ControlInformation::isDefined(std::string key) {
-	return !map[key].empty();
+bool ControlInformation::isDefined(const std::string& key) const {
+	return map.count(key) && !(map.at(key).empty());
 }
 
-ControlInformationType ControlInformation::getType() {
+ControlInformationType ControlInformation::getType() const {
     return this->type;
 }
 

commit c1dfb0940e125fda18c3722143dee6cb5cd4e5ef

Return default value instead of throwing.
Some parts of code rely on default return values rather than exceptions.
Fixes regression introduced in c96fed326b139ef8d8ddbdab836c5ed876b284d4.

hdt-lib/include/ControlInformation.hpp
@@ -89,15 +89,13 @@ public:
 
 	/** Get a property of the ControlInformation
 	 * @param key
-	 * @return
-	 * @throws std::out_of_range if the key is not found
+	 * @return the value, or the empty string if the key does not exist
 	 */
 	const std::string& get(const std::string& key) const;
 
 	/** Get a property of the ControlInformation as unsigned int
 	 * @param key
-	 * @return
-	 * @throws std::out_of_range if the key is not found
+	 * @return the value, or 0 if the key does not exist
 	 */
     uint64_t getUint(const std::string& key) const;
 

hdt-lib/src/hdt/ControlInformation.cpp
@@ -35,6 +35,8 @@
 
 using namespace std;
 
+const string EMPTY_STRING = "";
+
 namespace hdt {
 
 ControlInformation::ControlInformation() : type(UNKNOWN_CI) {}
@@ -201,7 +203,8 @@ void ControlInformation::setFormat(const std::string& format) {
 }
 
 const std::string& ControlInformation::get(const std::string& key) const {
-	return map.at(key);
+  std::map<string,string>::const_iterator it = map.find(key);
+  return it == map.end() ? EMPTY_STRING : it->second;
 }
 
 void ControlInformation::set(const std::string& key, const std::string& value) {
@@ -209,7 +212,8 @@ void ControlInformation::set(const std::string& key, const std::string& value) {
 }
 
 uint64_t ControlInformation::getUint(const std::string& key) const {
-    return strtoull(map.at(key).c_str(), NULL, 10);
+  std::map<string,string>::const_iterator it = map.find(key);
+  return it == map.end() ? 0 : strtoull(it->second.c_str(), NULL, 10);
 }
 
 void ControlInformation::setUint(const std::string& key, uint64_t value) {