e71d0da2b68625e76bdc3f721c79c34bc3ccf475
David Robillard <d@drobilla.net>
2017-04-24 21:32:25 +0200

    Clean up serd callbacks

--- hdt-lib/src/rdf/RDFParserSerd.hpp
+++ hdt-lib/src/rdf/RDFParserSerd.hpp
@@ -31,20 +31,21 @@ public:
 
 	void doParse(const char *fileName, const char *baseUri, RDFNotation notation, bool ignoreErrors, RDFCallback *callback);
 
-	friend SerdStatus hdtserd_process_triple(void* handle,
-	                                         SerdStatementFlags flags,
-	                                         const SerdNode*    graph,
-	                                         const SerdNode*    subject,
-	                                         const SerdNode*    predicate,
-	                                         const SerdNode*    object,
-	                                         const SerdNode*    object_datatype,
-	                                         const SerdNode*    object_lang);
+	friend SerdStatus hdtserd_on_statement(void               *handle,
+	                                       SerdStatementFlags  flags,
+	                                       const SerdNode     *graph,
+	                                       const SerdNode     *subject,
+	                                       const SerdNode     *predicate,
+	                                       const SerdNode     *object,
+	                                       const SerdNode     *datatype,
+	                                       const SerdNode     *lang);
 
-	friend SerdStatus hdtserd_prefixchanged(void* handle,const SerdNode* name, const SerdNode* uri);
-	friend SerdStatus hdtserd_basechanged(void* handle, const SerdNode* uri);
-	friend SerdStatus hdtserd_error_sink(void* handle, const SerdError* e);
-	RDFParserSerd();
-	virtual ~RDFParserSerd();
+	friend SerdStatus hdtserd_on_prefix(void           *handle,
+	                                    const SerdNode *name,
+	                                    const SerdNode *uri);
+
+	friend SerdStatus hdtserd_on_base(void *handle, const SerdNode *uri);
+	friend SerdStatus hdtserd_on_error(void *handle, const SerdError *e);
 };
 
 }
